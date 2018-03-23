b20d181870995c37a5f55c236dc6cac6061a90ef
jenniferlynjohnson <jenniferlynjohnson2000@gmail.com>
2018-03-23 14:27:30 -0700

    Create intro.rb

---
+++ intro.rb
@@ -0,0 +1,13 @@
+array=[ :c2, :c2, :e2, :f2]
+s= [0.25, 2, 0.75, 1.25]
+#this randomizes what note from the array is played. the numbers on line 8 are equivalent to the notes on line 6
+=begin
+3.times do
+  4.times do
+    p= choose([36, 36, 40,41])
+    play p
+    sleep s[i]
+    i=i+1
+  end
+  i=0
+end
