6372ea7383b043f52c458bbca4b40345c6815398
jenniferlynjohnson <jenniferlynjohnson2000@gmail.com>
2018-03-28 11:18:07 -0700

    Create ending.rb

---
+++ ending.rb
@@ -0,0 +1,32 @@
+live_loop :uhoh do
+  stop
+end
+
+live_loop :trumpet do #sleep=8
+  stop
+end
+
+live_loop :tuba do #sleep=8
+  stop
+end
+
+live_loop :bass do #sleep=8
+  stop
+end
+
+live_loop :drum do
+  stop
+end
+
+live_loop :ending do
+  
+  3.times do
+    sample beyonce_vocals ,amp: e
+    sleep 8
+    e = e - 1
+  end
+  stop
+end
+#=end
+
+
