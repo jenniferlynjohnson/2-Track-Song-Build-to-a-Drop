b0edaf79a856ad27fa76a9b56b887a20e3f8d5f7
jenniferlynjohnson <jenniferlynjohnson2000@gmail.com>
2018-03-28 11:20:46 -0700

    Create songtwo.rb

---
+++ songtwo.rb
@@ -0,0 +1,30 @@
+live_loop :uhoh do
+  sleep 8
+  j= choose([1, 3,])
+  sample beyonce_vocals ,amp: j
+  sleep 8
+end
+
+live_loop :trumpet do #sleep=8
+  trumpet
+  play :C5
+  sleep 0.5
+  trumpet
+  play :E5
+  sleep 0.5
+end
+
+live_loop :tuba do #sleep=8
+  tuba :Eb2
+  tuba :C2
+end
+
+live_loop :base do #sleep=8
+  bass
+end
+
+live_loop :drum do
+  sample :bd_boom
+  sleep 1
+end
+=end
